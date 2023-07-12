37468e0b4e2066a471002abe29e56b72db1bb5ff
Gustavo Junqueira Barbosa <gustavojunqueirab@gmail.com>
2023-07-12 11:14:51 -0300

    initial structure

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Dashboard() {
+  return (
+    <div>
+      <h1 className="font-extrabold">Dashboard</h1>
+      <Link href="/">Home</Link>
+    </div>
+  );
+}
